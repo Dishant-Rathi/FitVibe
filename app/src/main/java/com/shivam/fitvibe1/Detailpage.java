444ffa7bdbd293ad6ce780c6a25b9dc4aab133b8
Shivam Maloo <Shivammaloo12@gmail.com>
2023-05-20 11:31:13 +0530

    Initial Commit(Added parameters)

---
+++ app/src/main/java/com/shivam/fitvibe1/Detailpage.java
@@ -0,0 +1,14 @@
+package com.shivam.fitvibe1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Detailpage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detailpage);
+    }
+}
--- app/src/main/java/com/shivam/fitvibe1/MainActivity.java
+++ app/src/main/java/com/shivam/fitvibe1/MainActivity.java
@@ -2,13 +2,32 @@ package com.shivam.fitvibe1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnSearch;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnSearch=findViewById(R.id.btnSearch);
+
+
+
+
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,Detailpage.class);
+                startActivity(intent);
+
+
+            }
+
+        });
     }
 }
